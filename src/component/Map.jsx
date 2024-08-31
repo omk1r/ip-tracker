cba1f06d8d8a051634fabf4fc3b2a92b88b26ef6
Omkar <jadhavpatil2999@gmail.com>
2024-08-31 21:17:53 +0530

    image error solved

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -1,5 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import customMarkerIcon from '../assets/images/icon-location.svg';
+
+const customIcon = L.icon({
+  iconUrl: customMarkerIcon,
+  iconSize: [30, 30], // Size of the icon
+  iconAnchor: [19, 38], // Point of the icon which will correspond to marker's location
+  popupAnchor: [0, -38], // Point from which the popup should open relative to the iconAnchor
+});
 
 const Map = ({ lat, lng }) => (
   <MapContainer
@@ -12,7 +21,7 @@ const Map = ({ lat, lng }) => (
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution="&copy; OpenStreetMap contributors"
     />
-    <Marker position={[lat, lng]}>
+    <Marker position={[lat, lng]} icon={customIcon}>
       <Popup>Location found here.</Popup>
     </Marker>
   </MapContainer>
